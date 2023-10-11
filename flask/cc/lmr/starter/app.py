34d228373d03843434963f17db46194ee60961dc
Ashley Rivera <ashleyariv@gmail.com>
2023-10-11 15:38:55 -0400

    another finished cc

--- flask/cc/lmr/starter/app.py
+++ flask/cc/lmr/starter/app.py
@@ -20,6 +20,55 @@ db.init_app(app)
 def index():
     return "right/left"
 
+@app.get('/rights')
+def get_rights():
+    rights = Right.query.all()
+    right_list = []
+    for right in rights:
+        right_list.append(right.to_dict(rules = ('-middles',)))
+    return make_response(jsonify(right_list), 200)
+
+@app.get('/rights/<int:id>')
+def rights_by_id(id):
+    right = Right.query.filter(Right.id == id).first()
+    if not right:
+        return make_response(jsonify({'Error': 'This right does not exist.'}), 404)
+    return make_response(jsonify(right.to_dict()), 200)
+
+@app.post('/lefts')
+def post_left():
+    data = request.json
+    try:
+        new_left = Left(column = data.get('column'))
+        db.session.add(new_left)
+        db.session.commit()
+        return make_response(jsonify(new_left.to_dict()), 201)
+    except Exception as e:
+        return make_response(jsonify({'Error': 'Bad post request. ' + str(e)}), 405)
+
+@app.patch('/middles/<int:id>')
+def update_middle(id):
+    middle = Middle.query.filter(Middle.id == id).first()
+    if not middle:
+        return make_response(jsonify({'Error': 'This middle does not exist.'}), 404)
+    data = request.json
+    try:
+        for key in data:
+            setattr(middle, key, data[key])
+        db.session.add(middle)
+        db.session.commit()
+        return make_response(jsonify(middle.to_dict(rules = ('-left', '-right'))), 201)
+    except Exception as e:
+        return make_response(jsonify({'Error': 'Bad request. ' + str(e)}), 405)
+
+@app.delete('/lefts/<int:id>')
+def delete_left(id):
+    left = Left.query.filter(Left.id == id).first()
+    if not left:
+        return make_response(jsonify({'Error': 'This left does not exist.'}), 404)
+    db.session.delete(left)
+    db.session.commit()
+    return make_response(jsonify({}), 201)
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- flask/cc/lmr/starter/models.py
+++ flask/cc/lmr/starter/models.py
@@ -16,17 +16,51 @@ db = SQLAlchemy(metadata=metadata)
 
 class Left(db.Model, SerializerMixin):
     __tablename__ = "left_table"
+    serialize_rules = ('-middles.left',)
 
     id = db.Column(db.Integer, primary_key=True)
+    column = db.Column(db.String, unique = True, nullable = False)
 
+    middles = db.relationship('Middle', back_populates = 'left', cascade = 'all, delete')
+    rights = association_proxy('middles', 'Right')
+
+    @validates('column')
+    def valid_column(self, key, column):
+        if not column.isalpha():
+            raise ValueError('Please use letters only.')
+        return column
 
 class Middle(db.Model, SerializerMixin):
     __tablename__ = "middle_table"
+    serialize_rules = ('-left.middles', 'right.middles')
 
     id = db.Column(db.Integer, primary_key=True)
+    column = db.Column(db.String, nullable = False)
+    left_id = db.Column(db.Integer, db.ForeignKey('left_table.id'), nullable = False)
+    right_id = db.Column(db.Integer, db.ForeignKey('right_table.id'), nullable = False)
 
+    left = db.relationship('Left', back_populates = 'middles')
+    right = db.relationship('Right', back_populates = 'middles')
+
+    @validates('column')
+    def valid_column(self, key, column):
+        for i in column:
+            if not i in string.punctuation:
+                raise ValueError('Please use only punctuation.')
+        return column
 
 class Right(db.Model, SerializerMixin):
     __tablename__ = "right_table"
+    serialize_rules = ('-middles.right',)
 
     id = db.Column(db.Integer, primary_key=True)
+    column = db.Column(db.String, unique = True, nullable = False)
+
+    middles = db.relationship('Middle', back_populates = 'right', cascade = 'all, delete')
+    lefts = association_proxy('middles', 'Left')
+
+    @validates('column')
+    def valid_column(self, key, column):
+        if not column.isdigit():
+            raise ValueError('Please use numbers only.')
+        return column
